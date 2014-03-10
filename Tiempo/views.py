bb6768102c224746e1d5ef7215829b849e83eee2
David Pulloquinga <dapl@me.com>
2014-03-10 09:47:40 -0500

    1 edicion

---
+++ Tiempo/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+from django.template.loader import get_template
+from django.template import Context 
+from datetime import datetime
+
+# Create your views here.
+def hora_actual(request):
+	ahora=datetime.now()
+	t=get_template('tiempo.html')
+	c=Context({'hora':ahora})
+	html=t.render(c)
+	return HttpResponse(html)
